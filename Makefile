94cfcac2e9980220e9f96bdc574170a83e06d5ad
khcsce <49701587+khcsce@users.noreply.github.com>
2021-01-05 18:10:37 -0800

    Add files via upload
    first commit

---
+++ Makefile
@@ -0,0 +1,90 @@
+# NAME: Khoa Quach
+# EMAIL: khoaquachschool@gmail.com
+# ID: 105123806
+CC = gcc
+CFLAGS = -Wall -Wextra -g
+.SILENT:
+
+default:
+	$(CC) $(CFLAGS) -o lab0 lab0.c
+
+check: default basic_one basic_two basic_three invalid_option nonwritable_outputfile noexist_inputfile segfault_test catch_test 	
+	if [ -s log.txt ]; \
+	then \
+		echo "OH NO ERRORS EXISTS" \
+	        cat log.txt; \
+		rm -f log.txt; \
+	else \
+	        echo "Smoke Check SUCCESSFUL"; \
+	fi
+clean:
+	rm -f lab0 *.o *.txt *.tar.gz *~
+
+dist: 
+	tar -czvf lab0-105123806.tar.gz lab0.c Makefile *.png README
+#Smoke Check cases
+basic_one:
+	echo "hello word" > file.txt; \
+	./lab0 --input=file.txt --output=file2.txt; \
+	cmp file.txt file2.txt; \
+	if [[ $$? -ne 0 ]]; \
+	then \
+		echo "Error: input file to output file comparison failure" > log.txt; \
+	fi; \
+	rm -f file.txt file2.txt
+
+# no comparison between input file and output file
+basic_two:
+	echo "hello world" > file.txt; \
+	./lab0 --input=file.txt --output=file2.txt; \
+	if [[ $$? -ne 0 ]]; \
+	then \
+		echo "Error: input file to output file (no exit code = 0)" > log.txt; \
+	fi; \
+	rm -f file.txt file2.txt
+# basic stdin to std out (no files)
+basic_three:
+	echo "hello world" | ./lab0; \
+	if [[ $$? -ne 0 ]]; \
+	then \
+		echo "Error: copy stdin to stdout: no exit code = 0" > log.txt; \
+	fi;
+# invalid option
+invalid_option:
+	echo "hello world" | ./lab0 --what; \
+	if [[ $$? -ne 1 ]]; \
+	then \
+		echo "Error: unrecognizable option: no exit code = 1. Use --input=filename, --output=filename, --segfault, or --catch" > log.txt; \
+	fi;
+# nonwritable output file
+nonwritable_outputfile:
+	touch output.txt; chmod -w output.txt; \
+	./lab0 --output=output.txt; \
+	if [[ $$? -ne 3 ]]; \
+        then \
+                echo "Error: unwritable output file case: no exit code = 3" > log.txt; \
+        fi; \
+	rm -f output.txt;
+# input file that doesn't exists
+
+
+noexist_inputfile: 
+	./lab0 --input=noexist.txt; \
+	if [[ $$? -ne 2 ]]; \
+        then \
+                echo "Error: invalid input file (not existsing) case: no exit code = 2" > log.txt; \
+        fi;
+# segmentation fault and --check case
+segfault_test:
+	./lab0 --segfault; \
+	if [[ $$? -ne 139 ]]; \
+	then \
+		echo "Error: segfault case. no error code = 139" > log.txt; \
+	fi;
+catch_test:
+	./lab0 --segfault --catch; \
+	if [[ $$? -ne 4 ]]; \
+	then \
+		echo "Error: --catch case. no error code = 4" > log.txt; \
+	fi;
+
---
+++ lab0.c
@@ -0,0 +1,120 @@
+//NAME: Khoa Quach
+//EMAIL: khoaquachschool@gmail.com
+//ID: 105123806
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+/*
+Summary of exit codes
+0 ... copy successful
+1 ... unrecognized argument
+2 ... unable to open input file
+3 ... unable to open output file
+4 ... caught and received SIGSEGV
+*/
+/*-----------------Helper functions--------------------*/
+// Segfault
+void segfault_activate()
+{
+	char* seg_pointer = NULL;
+       	*seg_pointer = 's';
+}
+
+// For  case c (catch), call this function
+void sigsev_handler(int sig)
+{
+	if(sig == SIGSEGV) { 
+		fprintf(stderr, "Segmentation fault (SIGSEGV handler that catches the segmentation fault)\n ");
+		exit(4); // exit(2) with a return code of 4.
+
+	}
+}
+
+void read_write()
+{
+	char buf;
+	while(read(0,&buf,1) > 0)
+	{
+		if(write(1,&buf,1) < 0)
+		{
+			fprintf(stderr, "--output error: Cannot write to output file: %s\n",strerror(errno));
+			exit(3);
+		}
+	}
+}
+int main(int argc, char **argv){
+	// Input and Output files
+	int ifd;
+	int ofd;
+	//http://web.cs.ucla.edu/~harryxu/courses/111/winter21/ProjectGuide/fd_juggling.html
+	// intialize to zero
+	int seg_flag = 0;
+	//int catch = 0;
+	int choice = 0; // used with getopt_long
+	// Command line options, used with getopt_long
+	static struct option options[] = 
+	{
+		{"input", 1, 0, 'i'},
+		{"output", 1, 0, 'o'},
+		{"segfault",0,0,'s'},
+		{"catch", 0, 0, 'c'},
+		{0,0,0,0}
+	};
+
+	while((choice = getopt_long(argc, argv, "", options,NULL))!= -1)
+	{
+		switch(choice){
+			case 'i':
+				ifd = open(optarg, O_RDONLY);
+				if (ifd >= 0) {
+					close(0);
+					dup(ifd);
+					close(ifd);
+				}
+				else {
+					fprintf(stderr, "--input option error: Could not open file %s ", optarg);
+					fprintf(stderr, "%s\n", strerror(errno));
+					exit(2);
+					
+				}
+				break;
+			// case i end
+			case 'o':
+				ofd = creat(optarg,0666);
+				if (ofd >= 0) {
+					close(1);
+					dup(ofd);
+					close(ofd);
+				}
+				else {
+					fprintf(stderr, "--output option error: Could not create outputfile %s ", optarg);
+					fprintf(stderr, "%s\n", strerror(errno));
+					exit(3);
+					
+				}
+				break;
+			// case o end
+			case 's':
+				seg_flag = 1;
+				break;
+			case 'c':
+				signal(SIGSEGV, sigsev_handler);
+				break;
+			case '?':
+				fprintf(stderr, "unrecognized argument! Correct usage: ./lab0 --input filename --output filename, --segfault, --catch\n");
+				exit(1);
+			default:
+				fprintf(stderr, "unrecognized argument! Correct usage: ./lab0 --input filename --output filename, --segfault, --catch\n");
+				exit(1);
+		}// switch
+	}// while
+	if (seg_flag)
+		segfault_activate();
+	read_write();
+	exit(0);
+} // main
